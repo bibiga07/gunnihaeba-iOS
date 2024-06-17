c532ece7a227c201d2507f259c2b7f2a10515c30
lsh1202 <vic071202@gmail.com>
2024-06-17 12:20:23 +0900

    :tada: :: 프로젝트 만듦

---
+++ gunnihaeba-iOS/gunnihaeba-iOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  gunnihaeba-iOS
+//
+//  Created by hyk on 6/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ gunnihaeba-iOS/gunnihaeba-iOS/gunnihaeba_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  gunnihaeba_iOSApp.swift
+//  gunnihaeba-iOS
+//
+//  Created by hyk on 6/17/24.
+//
+
+import SwiftUI
+
+@main
+struct gunnihaeba_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
