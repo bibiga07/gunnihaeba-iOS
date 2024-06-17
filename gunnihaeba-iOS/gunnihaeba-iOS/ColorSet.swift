dde3e3b5733ddfb6434a5642038acfb97b69d013
bibiga07 <130065479+bibiga07@users.noreply.github.com>
2024-06-17 23:56:01 +0900

    ✨ Color Set

---
+++ gunnihaeba-iOS/gunnihaeba-iOS/ColorSet.swift
@@ -0,0 +1,27 @@
+//
+//  ColorSet.swift
+//  gunnihaeba-iOS
+//
+//  Created by bibiga on 6/17/24.
+//
+
+import SwiftUI
+ 
+extension Color {
+  init(hex: String) {
+    let scanner = Scanner(string: hex)
+    _ = scanner.scanString("#")
+    
+    var rgb: UInt64 = 0
+    scanner.scanHexInt64(&rgb)
+    
+    let r = Double((rgb >> 16) & 0xFF) / 255.0
+    let g = Double((rgb >>  8) & 0xFF) / 255.0
+    let b = Double((rgb >>  0) & 0xFF) / 255.0
+    self.init(red: r, green: g, blue: b)
+  }
+}
+
+extension Color {
+    static let boxColor = Color(hex: "#D9D9D9")
+}
