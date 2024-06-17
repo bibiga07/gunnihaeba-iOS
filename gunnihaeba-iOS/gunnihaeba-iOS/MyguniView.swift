c79f8c28f9e70c77831b042d337768deb91aeb78
bibiga07 <130065479+bibiga07@users.noreply.github.com>
2024-06-17 12:25:36 +0900

    🎉 Start project

---
+++ gunnihaeba-iOS/gunnihaeba-iOS/MyguniView.swift
@@ -0,0 +1,18 @@
+//
+//  MyguniView.swift
+//  gunnihaeba-iOS
+//
+//  Created by bibiga on 6/17/24.
+//
+
+import SwiftUI
+
+struct MyguniView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyguniView()
+}
